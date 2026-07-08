cc9f932ea8811c00b89fe40912d4aaa152412261
AlexDarev <151463648+AlexDarev@users.noreply.github.com>
2026-07-08 11:04:17 +0100

    Update script.js

--- frontend/animations/clip-path-scroll-reveal/script.js
+++ frontend/animations/clip-path-scroll-reveal/script.js
@@ -1,1 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Image Scale
+    gsap.from(".image-reveal img", {
+        duration: 1,
+        scale: 1.5,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: ".image-reveal",
+            start: "top 40%",
+            once: true
+        }
+    });
+
+    // Clip-path Reveal
+    gsap.from(".image-reveal", {
+        duration: 1,
+        clipPath: "polygon(0 0, 100% 0, 100% 0%, 0 0%)",
+        ease: "power1.out",
+        scrollTrigger: {
+            trigger: ".image-reveal",
+            start: "top 40%",
+            once: true
+        }
+    });
+
+});
